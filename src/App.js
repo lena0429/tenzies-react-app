6d26e02fe903bbb31cd0cea6ccdfc204c72e5946
Lena Hou <lenahou2013@gmail.com>
2022-04-17 18:24:12 -0400

    creating an array that holds 10 random numbers

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
+import React, { useState } from 'react';
 import './style.css';
 import Die from './components/Die';
 
+
 function App() {
+  const [ dice, setDice ] = useState(allNewDice())
+
+  console.log(dice)
+
+  function allNewDice(){
+    let arr = [];
+
+    for (let i = 0; i < 10; i++){
+      arr.push(Math.ceil(Math.random() * 6))
+    }
+    return arr;
+  }
+
+  const diceArray = dice.map((num, idx) => <Die value={num} />) 
+
   return (
     <main>
       <div className="dice-container">
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
+          {diceArray}
       </div>
     </main>
   );
--- src/components/Die.js
+++ src/components/Die.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Die(props){
     return(
         <div className="die-face">
-            <h2>{props.value}</h2>
+            <h2 className="die-num">{props.value}</h2>
         </div>
     )
 }
