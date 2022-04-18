63fba38842d5d6c80e45f15688d35bb2563c3e9f
Lena Hou <lenahou2013@gmail.com>
2022-04-17 21:21:31 -0400

    adding title and instructions

--- src/App.js
+++ src/App.js
@@ -44,6 +44,8 @@ function App() {
 
   return (
     <main>
+      <h1 className="title">React Tenzies Game</h1>
+      <p className="instructions"> Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className="dice-container">
           {diceArray}
       </div>
